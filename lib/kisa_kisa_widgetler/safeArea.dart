59603d976fae5b05d8b53d4b7d9ae41c5984b577
Necip <nexco@hotmail.com>
2019-07-09 04:24:52 +0300

    SafeArea Eklendi, AnaSayfa Düzenlendi

---
+++ lib/kisa_kisa_widgetler/safeArea.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class SafeAreaDemo extends StatefulWidget {
+  @override
+  _SafeAreaDemoState createState() => _SafeAreaDemoState();
+}
+
+class _SafeAreaDemoState extends State<SafeAreaDemo> {
+  bool _acikmi = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: SafeArea(
+        // Sağ, Sol, Alt, Üst olarak belirtebileceğimiz gibi
+        // belirtmediğimiz zaman hepsini kapsar
+        top: _acikmi,
+        bottom: _acikmi,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            _ustTaraf(),
+            _ortaTaraf(),
+            _altTaraf(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _ustTaraf() {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.wb_sunny),
+        title: Text(
+          "SafeArea Örnek",
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        trailing: Icon(Icons.wb_cloudy),
+      ),
+    );
+  }
+
+  /// Switch durumuna göre safeAre'yı açıp kapayacak
+  Widget _ortaTaraf() {
+    return Card(
+      child: SwitchListTile(
+        secondary: Icon(Icons.all_inclusive),
+        onChanged: (deger) {
+          setState(() {
+            _acikmi = deger;
+          });
+        },
+        value: _acikmi,
+        title: Text(
+          "Aç - Kapa",
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _altTaraf() {
+    return Card(
+      child: ListTile(
+        onTap: () => Navigator.pop(context),
+        leading: Icon(Icons.arrow_back),
+        title: Text(
+          "Geri Git",
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
