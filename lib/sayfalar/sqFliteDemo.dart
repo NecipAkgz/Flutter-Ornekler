d161251dd317f67df87094d1fdfa55487556e337
Necip <nexco@hotmail.com>
2019-07-30 22:22:42 +0300

    sqlfite create database kısayol

--- lib/sayfalar/sqFliteDemo.dart
+++ lib/sayfalar/sqFliteDemo.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'dart:async';
 import 'package:path/path.dart';
@@ -30,7 +28,7 @@ class _SQLiteDemoState extends State<SQLiteDemo> {
       body: FutureBuilder<List<Client>>(
         // future olarak database sınıfımızdaki bütün müşterileri getir
         // adlı methodunu verdik
-        future: DBProvider.db.getAllClients(),
+        future: DBHelper().getAllClients(),
         builder: (BuildContext context, AsyncSnapshot<List<Client>> snapshot) {
           // eğer verdiğimiz future içerisinde veri var ise bunları
           if (snapshot.hasData) {
@@ -42,7 +40,7 @@ class _SQLiteDemoState extends State<SQLiteDemo> {
                   key: UniqueKey(),
                   background: Container(color: Colors.red),
                   onDismissed: (direction) {
-                    DBProvider.db.deleteClient(item.id);
+                    DBHelper().deleteClient(item.id);
                   },
                   child: ListTile(
                     title: Text(item.firstName),
@@ -50,7 +48,7 @@ class _SQLiteDemoState extends State<SQLiteDemo> {
                     leading: Text(item.id.toString()),
                     trailing: Checkbox(
                       onChanged: (bool value) {
-                        DBProvider.db.blockOrUnblock(item);
+                        DBHelper().blockOrUnblock(item);
                         setState(() {});
                       },
                       value: item.blocked,
@@ -73,7 +71,7 @@ class _SQLiteDemoState extends State<SQLiteDemo> {
           // listemizin uzunluğu sayısı kadar rasgele sayı üretip
           // o sayıya denk gelen müşteriyi database'e ekleyecek
           Client rnd = testClients[math.Random().nextInt(testClients.length)];
-          await DBProvider.db.newClient(rnd);
+          await DBHelper().newClient(rnd);
           setState(() {});
         },
       ),
@@ -86,51 +84,38 @@ class _SQLiteDemoState extends State<SQLiteDemo> {
 /* -------------------------------------------------------------------------- */
 
 /// Databasehelper.dart
-class DBProvider {
-  DBProvider._();
-  static final DBProvider db = DBProvider._();
 
-  static Database _database;
+class DBHelper {
+  /// Dabase'imizi ilgili konuma oluşturup, oluşturduğu database'i istediğimizde dönen method
+  static Future<Database> database() async {
+    // işletim sistemine göre varsayılan database oluşturabileceğimiz konumu alacak
+    final dbPath = await getDatabasesPath();
 
-  // database nesnemizi çağırdığımızda bize gerekli işlemleri yapacak
-  // yok ise oluştur var ise onu dön
-  Future<Database> get database async {
-    if (_database != null) return _database;
-
-    // oluşmuş database yoksa oluştur onu dönder
-    _database = await initDB();
-    return _database;
-  }
-
-  initDB() async {
-    // Klasör yolunu bul
-    Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    // Bulunan klasör yolu ve belirttiğimiz database ismi ile bir dosya yolu yarat
-    String path = join(documentsDirectory.path, "TestDB.db");
-    // Verdiğimiz dosya yolu, version bilgileri ışığında database'imizi yarat
-    return await openDatabase(
-      path,
-      version: 1,
-      onOpen: (db) {},
-      onCreate: _createDB,
-    );
-  }
-
-  // Database oluşturulurkenki satır, sütun isimleri (Klasik SQL)
-  Future _createDB(Database db, int version) async {
-    await db.execute("CREATE TABLE Client ("
+    // Client isminde database table oluşturacak rawSql komutumuzu tutan değişken
+    // karışık görünmesin diye buraya yazdım, execute ederken kullanıcaz
+    const dbSQL = "CREATE TABLE Client ("
         "id INTEGER PRIMARY KEY,"
         "first_name TEXT,"
         "last_name TEXT,"
         "blocked BIT"
-        ")");
+        ")";
+
+    // ve SqlFlite ile gelen openDatabase methodu ile database'i oluşturup onu dönüyoruz.
+    return openDatabase(
+      // database'in oluşturulacağı konum; yukarıda aldığımız varsayalın database konumu ve
+      // oluşturmak istediğimiz isimde dosyamızın adını verip, o konuma o isimde oluşturmasını istedik
+      join(dbPath, "TestDB.db"), // -> 'varsayılanKonum/TestDb.db'
+      // verdiğimiz SQL komutu ve version numarası ile database'imizi oluşturmasını istedik
+      onCreate: (db, version) => db.execute(dbSQL),
+      version: 1,
+    );
   }
 
   /// Yeni Kayıt
   newClient(Client newClient) async {
-    final db = await database;
+    final db = await database();
     // Client isimli tabloya parametre olarak verdiğimiz yeni müşteriyi
-    // map'e çevir ve database'imize ekle
+    // map'e çevir ve database()'imize ekle
     var sonuc = await db.insert("Client", newClient.toMap());
     // geri dönüş değerini dönder, ekleme olumlu olursa, eklendi "id"'yi dönecek
     return sonuc;
@@ -138,8 +123,8 @@ class DBProvider {
 
   /// Verilen "id" değerine göre elemanı getir
   getClient(int id) async {
-    final db = await database;
-    // parametre olarak verdiğimiz "id" ye göre database'den ilgili elemanı getircek
+    final db = await database();
+    // parametre olarak verdiğimiz "id" ye göre database()'den ilgili elemanı getircek
     var sonuc = await db.query("Client", where: "id = ?", whereArgs: [id]);
     // eğer sonuc boş değilse bulduğu ilk elemanı dön, boş işe null dön
     return sonuc.isNotEmpty ? Client.fromMap(sonuc.first) : Null;
@@ -147,7 +132,7 @@ class DBProvider {
 
   /// bütün elemanları getir
   Future<List<Client>> getAllClients() async {
-    final db = await database;
+    final db = await database();
     var sonuc = await db.query("Client");
     List<Client> list =
         sonuc.isNotEmpty ? sonuc.map((c) => Client.fromMap(c)).toList() : [];
@@ -156,7 +141,7 @@ class DBProvider {
 
   /// sadece bloklanmış elemanları liste şeklinde dönecek
   Future<List<Client>> getBlockedClients() async {
-    final db = await database;
+    final db = await database();
     var sonuc = await db.query("Client", where: "blocked = ? ", whereArgs: [1]);
     List<Client> list =
         sonuc.isNotEmpty ? sonuc.map((c) => Client.fromMap(c)).toList() : [];
@@ -165,7 +150,7 @@ class DBProvider {
 
   /// var olan müşteriyi günceller
   updateClient(Client newClient) async {
-    final db = await database;
+    final db = await database();
     var sonuc = await db.update("Client", newClient.toMap(),
         where: "id = ?", whereArgs: [newClient.id]);
     return sonuc;
@@ -173,7 +158,7 @@ class DBProvider {
 
   /// Verilen müşteriyi bloklar veya açar
   blockOrUnblock(Client client) async {
-    final db = await database;
+    final db = await database();
     Client blocked = Client(
         id: client.id,
         firstName: client.firstName,
@@ -186,13 +171,13 @@ class DBProvider {
 
   /// verilen id değerine göre müşteriyi siler
   deleteClient(int id) async {
-    final db = await database;
+    final db = await database();
     db.delete("Client", where: "id = ?", whereArgs: [id]);
   }
 
   /// bütün müşterileri siler
   deleteAll() async {
-    final db = await database;
+    final db = await database();
     db.rawDelete("Delete * from Client");
   }
 }
